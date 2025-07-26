d4dc9bf7f6ca702167b313eb2e241c6419e9e8e2
unknown <kerolloseid34@gmail.com>
2025-07-26 14:16:08 +0300

    Add custom scroll view

--- lib/Features/home/Presentation/view/widgets/book_detalis_view_body.dart
+++ lib/Features/home/Presentation/view/widgets/book_detalis_view_body.dart
@@ -13,55 +13,66 @@ class BookDetalisViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Column(
-        children: [
-          CustomBookDetalisAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: width * 0.24,
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 25,
             ),
-            child: CustomItemImage(),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'Harry Potter and \nthe Globet of Fire',
-            style: StyleS.textStyles30.copyWith(
-              fontSize: 25,
+            child: Column(
+              children: [
+                CustomBookDetalisAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: width * 0.24,
+                  ),
+                  child: CustomItemImage(),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Harry Potter and \nthe Globet of Fire',
+                  style: StyleS.textStyles30.copyWith(
+                    fontSize: 25,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    'J.K. Rowling',
+                    style: StyleS.textStyles20.copyWith(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const BookRating(
+                  mainAxisAlignment:
+                      MainAxisAlignment.center,
+                ),
+                const SizedBox(height: 20),
+                const ActionButton(),
+                // save to area
+                Expanded(child: const SizedBox(height: 50)),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You Can also like',
+                    style: StyleS.textStyles16.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const SimilarBooksListViewImages(),
+                const SizedBox(height: 10),
+              ],
             ),
           ),
-          const SizedBox(height: 6),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              'J.K. Rowling',
-              style: StyleS.textStyles20.copyWith(
-                color: Colors.grey,
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(height: 20),
-          const ActionButton(),
-          const SizedBox(height: 50),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You Can also like',
-              style: StyleS.textStyles16.copyWith(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          const SimilarBooksListViewImages(),
-          const SizedBox(height: 10),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
