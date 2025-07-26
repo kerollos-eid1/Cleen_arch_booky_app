67f99f99f08df11441812e48d4ad75be492495cf
unknown <kerolloseid34@gmail.com>
2025-07-26 14:45:04 +0300

    create search views

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,10 +1,13 @@
 import 'package:clean_arch_booky_app/Features/home/Presentation/view/Home_view.dart';
 import 'package:clean_arch_booky_app/Features/home/Presentation/view/book_detalis_views.dart';
+import 'package:clean_arch_booky_app/Features/search/presentation/views/search_view.dart';
 import 'package:clean_arch_booky_app/Features/splash/presentation/views/Splash_View.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kBookDetailsVeiw = '/bookDetailsVeiw';
+  static const kHomeView = '/homeView';
+  static const kSearchView = '/searchVeiw';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -12,12 +15,16 @@ abstract class AppRouter {
         builder: (context, state) => SplashView(),
       ),
       GoRoute(
-        path: '/homeView',
+        path: kHomeView,
         builder: (context, state) => HomeView(),
       ),
       GoRoute(
-        path: '/bookDetailsVeiw',
+        path: kBookDetailsVeiw,
         builder: (context, state) => BookDetalisViews(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => SearchView(),
       ),
     ],
   );
