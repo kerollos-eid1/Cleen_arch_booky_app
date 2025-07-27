a48958073afbacbbdb63b320585dcc74648c1344
unknown <kerolloseid34@gmail.com>
2025-07-27 20:58:48 +0300

    fetch newest books use case

---
+++ lib/Features/home/Donain/use_case/fetch_newest_books_use_case.dart
@@ -0,0 +1,21 @@
+import 'package:clean_arch_booky_app/Features/home/Donain/entities/home_entity.dart';
+import 'package:clean_arch_booky_app/Features/home/Donain/repo/home_repo.dart';
+import 'package:clean_arch_booky_app/core/errors/failure.dart';
+import 'package:clean_arch_booky_app/core/use_case/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+// This use case is responsible for fetching the newest books from the repository.
+// It extends a generic UseCase class that defines the contract for use cases in the application.
+class FetchNewestBooksUseCase
+    extends UsCase<List<HomeEntity>, NoParams> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<HomeEntity>>> call([
+    NoParams? params,
+  ]) async {
+    return await homeRepo.fetchNewestBooks();
+  }
+}
