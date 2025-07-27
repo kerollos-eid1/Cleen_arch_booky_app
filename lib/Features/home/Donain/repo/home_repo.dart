f01da3af23368cf1fea9d7663165bfba03270480
unknown <kerolloseid34@gmail.com>
2025-07-27 18:46:54 +0300

    add either class

--- lib/Features/home/Donain/repo/home_repo.dart
+++ lib/Features/home/Donain/repo/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:clean_arch_booky_app/Features/home/Donain/entities/home_entity.dart';
+import 'package:clean_arch_booky_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<HomeEntity>> fetchFeaturedBooks();
-  Future<List<HomeEntity>> fetchNewestBooks();
+  Future<Either< Failure, List<HomeEntity>>> fetchFeaturedBooks();
+  Future<Either< Failure, List<HomeEntity>>> fetchNewestBooks();
 }
