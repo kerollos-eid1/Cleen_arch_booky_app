66dec9995b20d776804bf729c5928b43754363e9
unknown <kerolloseid34@gmail.com>
2025-08-01 17:25:18 +0300

    Fetch featured books home repo impl

--- lib/Features/home/Data/repos/home_repos_impl.dart
+++ lib/Features/home/Data/repos/home_repos_impl.dart
@@ -1,13 +1,32 @@
+import 'package:clean_arch_booky_app/Features/home/Data/data_source/home_local_data_source.dart';
+import 'package:clean_arch_booky_app/Features/home/Data/data_source/home_remote_data_source.dart';
 import 'package:clean_arch_booky_app/Features/home/Domain/entities/book_entity.dart';
 import 'package:clean_arch_booky_app/Features/home/Domain/repo/home_repo.dart';
 import 'package:clean_arch_booky_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeReposImpl extends HomeRepo{
+class HomeReposImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+  HomeReposImpl({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      // First, try to fetch books from local data source
+      // If local data source is empty, then fetch from remote data source
+      var booklist = homeLocalDataSource.fetchFeaturedBooks();
+      if (booklist.isNotEmpty) {
+        return right(booklist);
+      }
+      // Fetching books from remote data source
+      var books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
 
   @override
